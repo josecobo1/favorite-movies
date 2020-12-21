d5bd9283ad9678e65bbd0a84a3857a3bd99cf41c
rglepe <rglepe@gmail.com>
2020-12-21 08:55:55 +0100

    testing added

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 
 
-/* const api_url = 'https://api.themoviedb.org/3/movie/popular?api_key=20e88e4279bd29045f53af4704afff80'; */
+
 const api_url ='https://jsonplaceholder.typicode.com'
 export const environment = {
   production: false,
